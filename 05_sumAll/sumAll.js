5a0c5261ae35dc61427f2c0d46f3643d7aa6de1d
Sagnik <sag.muk200800@gmail.com>
2025-04-06 18:22:37 +0530

    commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(arg1,arg2) {
 
+    if(Number.isInteger(arg1)==false || Number.isInteger(arg2)==false || arg1<0 || arg2<0)
+        return 'ERROR'
+
+    else
+    {
+        var addedvalue=0;
+
+        if(arg1<arg2)
+        {
+            for(let i=arg1;i<=arg2;i++)
+                addedvalue+=i;
+        }
+        else
+        {
+            for(let i=arg2;i<=arg1;i++)
+                addedvalue+=i;
+        }
+
+
+        return addedvalue
+    }
 };
 
 // Do not edit below this line
