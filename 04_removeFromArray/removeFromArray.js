23f3a8c25b3f839ad5d994a132a6b02a9418e0ba
Sagnik <sag.muk200800@gmail.com>
2025-04-06 18:09:55 +0530

    Exercise 3 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    var filteredarray;
+
+    filteredarray=array.filter(items=>{
+        return !args.includes(items)
+    });
+
+    return filteredarray
 };
 
 // Do not edit below this line
