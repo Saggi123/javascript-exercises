1951ff407304e2c30afbbb21b9f5c47e3b69e9af
Sagnik <sag.muk200800@gmail.com>
2025-04-06 17:59:51 +0530

    Exercise 3 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+if(word=='')
+    return ''
 
+else{
+    var reversedstring='';
+
+    for(let i=word.length-1;i>=0;i--)
+        reversedstring+=word.charAt(i)
+
+    return reversedstring;
+}
 };
 
 // Do not edit below this line
