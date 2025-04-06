25c44bbb0c01f2aef903161a03f8fb1a3735653f
Sagnik <sag.muk200800@gmail.com>
2025-04-06 15:33:04 +0530

    Exercise 2 Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(word,count) 
+{
+    if(count<0)
+        return 'ERROR';
+    else if(word=='')
+        return '';
+    else if(count==0)
+        return ''
+    else
+    {
+        var concatword=word;
+        for(let i=1;i<count;i++)
+        concatword+=word;
+        
+        return concatword
+    }
 };
 
 // Do not edit below this line
